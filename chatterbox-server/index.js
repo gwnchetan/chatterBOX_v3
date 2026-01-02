fc3789adfac4bf0b24d770031058f8cda6dd52a6
Chetan Sakre <csakre634@gmail.com>
2026-01-02 22:41:59 +0530

    Fix app undefined error in server index.js

--- chatterbox-server/index.js
+++ chatterbox-server/index.js
@@ -1,6 +1,15 @@
+const express = require('express');
+const cors = require('cors');
 const compression = require('compression');
+const ConnectDB = require('./db');
+require('dotenv').config();
 
-// ...
+ConnectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const path = require('path');
 
 app.use(compression()); // Compress all routes
 app.use(cors());

b46db2e86242b82979e92ced7138416c1ac150e1
Chetan Sakre <csakre634@gmail.com>
2026-01-02 21:39:17 +0530

    Fix dotenv version and add env debug logs

--- chatterbox-server/db.js
+++ chatterbox-server/db.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    // Debugging: Log available environment variables (keys only for security)
+    console.log("Environment Variables Keys:", Object.keys(process.env));
+    console.log("MONGO_URI defined:", !!process.env.MONGO_URI);
+
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is missing from environment variables");
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
